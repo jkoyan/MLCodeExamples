a0aeb08b9ec165a7af81395ba130cc9ad9a7c4fe
Jimmy Koyan <jimmy.koyan@gmail.com>
2016-06-26 16:26:39 -0400

    First commit, contains python code to train a Naive-Bayes classifier and perform classification on the iris dataset.

---
+++ nbmodel/nbclassifier.py
@@ -0,0 +1,61 @@
+__author__ = 'jkoyan'
+
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.naive_bayes import  GaussianNB
+from sklearn.metrics import accuracy_score,confusion_matrix
+from sklearn.cross_validation import train_test_split
+#import pandas as pd
+import  numpy as np
+from StringIO import StringIO
+
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+
+
+clf = GaussianNB()
+
+clf.fit(X_train,y_train)
+
+y_predict = clf.predict(X_test)
+
+score = accuracy_score(y_test,y_predict,normalize=False)
+
+print("Total number of correctly classified observations: {0} out of {2} observations, Accuracy of the predictions: {1}").format(score,score/float(len(y_test)),len(y_test))
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(iris.target_names))
+    plt.xticks(tick_marks, iris.target_names, rotation=45)
+    plt.yticks(tick_marks, iris.target_names)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+#Compute confusion matrix
+cm = confusion_matrix(y_test,y_predict)
+np.set_printoptions(precision=2)
+print('Confusion matrix, without normalization')
+print(cm)
+#df_cm = pd.crosstab(y_test,y_predict)
+#print(df_cm)
+plt.figure()
+plot_confusion_matrix(cm)
+
+# Normalize the confusion matrix by row (i.e by the number of samples
+# in each class)
+cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+print('Normalized confusion matrix')
+print(cm_normalized)
+plt.figure()
+plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
+plt.show()
---
+++ nbmodel/r-code/plots.r
@@ -0,0 +1,10 @@
+library(fpp)
+library(dplyr)
+library(corrplot)
+iris <- tbl_df(read.csv('//Users//GraysTECH//BigQLabs//ml//MLCodeExamples//blog-post1//data//iris.data.txt',header=TRUE,sep=','))
+pairs(iris[-5], main = "Fisher's Iris Data -- 3 species",pch = 21, bg = c("red", "green3", "blue")[unclass(iris$iris_class)],oma=c(4,4,6,12))
+par(xpd=TRUE)
+legend(0.85, 0.7, as.vector(unique(iris$iris_class)),
+       fill=c("red", "green3", "blue"))
+> i <- cor(iris[-5])
+> corrplot(i,method='number')
